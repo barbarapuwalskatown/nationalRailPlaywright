2cbe47bca593cf16e87729e6e74ebb0e6e04c492
barbarapuwalskatown <164621498+barbarapuwalskatown@users.noreply.github.com>
2025-01-10 15:39:20 +0100

    grouping tests and tags

--- tests/planJourney.spec.ts
+++ tests/planJourney.spec.ts
@@ -6,64 +6,82 @@ test.beforeEach(async({page}) => {
     await page.getByRole('button', {name: "Accept"}).first().click()
 })
 
-test('search journey with station names', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const [searchedJourney, departureStationName, arrivalStationName] = await planJourneyPage.searchJourneyStationNames()
+test.describe('routes', { tag: '@PlanJourney'}, () => {
 
-    expect(searchedJourney).toContain(departureStationName)
-    expect(searchedJourney).toContain(arrivalStationName)
+    test('search journey', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const [searchedJourney, departureStationName, arrivalStationName] = await planJourneyPage.searchJourneyStationNames()
+    
+        expect(searchedJourney).toContain(departureStationName)
+        expect(searchedJourney).toContain(arrivalStationName)
+    })
+
+    test('do not change at', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const [searchedJourney, notChangeStationName] = await planJourneyPage.notChangeAt()
+    
+        expect(searchedJourney).toContain(`Do Not Change At ${notChangeStationName}`)
+    })
+
 })
 
-test('add adults', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const [searchedJourney, numberOfAdults] = await planJourneyPage.searchJourneyWithMoreAdults()
+test.describe('calendar', { tag: '@PlanJourney'}, () => {
 
-    expect(searchedJourney).toContain(`${numberOfAdults} Adults`)
+    test('select date within thirty days', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const [searchedJourney, fullExpectedDate] = await planJourneyPage.selectDateWithinThirtyDays()
+    
+        expect(searchedJourney).toContain(fullExpectedDate)
+    })
+
 })
 
-test('add children', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const [searchedJourney, numberOfChildren] = await planJourneyPage.searchJourneyWithMoreChildren()
+test.describe ('add passengers', { tag: '@PlanJourney'}, () => {
 
-    if(numberOfChildren === "0") {
-        expect(searchedJourney).not.toContain(`Child`)
-    } else {
-        expect(searchedJourney).toContain(`${numberOfChildren} Child`)
-    }
+    test('add adults', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const [searchedJourney, numberOfAdults] = await planJourneyPage.searchJourneyWithMoreAdults()
+
+        expect(searchedJourney).toContain(`${numberOfAdults} Adults`)
+    })
+
+    test('add children', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const [searchedJourney, numberOfChildren] = await planJourneyPage.searchJourneyWithMoreChildren()
+
+        if(numberOfChildren === "0") {
+            expect(searchedJourney).not.toContain(`Child`)
+        } else {
+            expect(searchedJourney).toContain(`${numberOfChildren} Child`)
+        }
+        
+    })
+})
+
+test.describe('railcards', { tag: '@PlanJourney'}, () => { 
+
+    test('add railcard', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const [railcardsAmount, searchedJourney] = await planJourneyPage.selectAmountOfRailcards()
+    
+        expect(searchedJourney).toContain(`${railcardsAmount} Railcard`)
+    })
     
 })
 
-test('search fastest trains', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const searchedJourney = await planJourneyPage.fastestTrainsOnly()
+test.describe('journey options', { tag: '@PlanJourney'}, () => {
 
-    expect(searchedJourney).toContain(`Fastest trains only`)
-})
+    test('search fastest trains', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const searchedJourney = await planJourneyPage.fastestTrainsOnly()
+    
+        expect(searchedJourney).toContain(`Fastest trains only`)
+    })
 
-test('do not change at', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const [searchedJourney, notChangeStationName] = await planJourneyPage.notChangeAt()
-
-    expect(searchedJourney).toContain(`Do Not Change At ${notChangeStationName}`)
-})
-
-test('select date within thirty days', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const [searchedJourney, fullExpectedDate] = await planJourneyPage.selectDateWithinThirtyDays()
-
-    expect(searchedJourney).toContain(fullExpectedDate)
-})
-
-test('add railcard', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const [railcardsAmount, searchedJourney] = await planJourneyPage.selectAmountOfRailcards()
-
-    expect(searchedJourney).toContain(`${railcardsAmount} Railcard`)
-})
-
-test('add extra time', async({page}) => {
-    const planJourneyPage = new PlanJourney(page)
-    const [searchedJourney,extraTimeText] = await planJourneyPage.addExtraTime()
-
-    expect(searchedJourney).toContain(`${extraTimeText}`)
+    test('add extra time', async({page}) => {
+        const planJourneyPage = new PlanJourney(page)
+        const [searchedJourney,extraTimeText] = await planJourneyPage.addExtraTime()
+    
+        expect(searchedJourney).toContain(`${extraTimeText}`)
+    })
 })
